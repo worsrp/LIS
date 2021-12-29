609c7557fc9395a334aef8c43a79c492f477c982
khampanpare88 <khampanpare88@gmail.com>
2021-12-29 18:36:13 +0700

    createpost no picture and location

--- server/index.js
+++ server/index.js
@@ -1,8 +1,48 @@
-import express from 'express';
-import mysql from 'mysql';
-import cors from 'cors';
+//import express from 'express';
+//import mysql from 'mysql';
+//import cors from 'cors';
 
+const express = require("express");
+const bodyPaeser = require('body-parser');
+const cors =require('cors')
 const app = express();
+const mysql = require('mysql');
 
+
+
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "LISDatabase",
+});
+
+app.use(bodyPaeser.urlencoded({extended: true}))
 app.use(cors());
 app.use(express.json());
+
+app.post("/createpost", (req, res) =>{
+    //test
+    const USER_ID = 12345;
+    const ITEM_ID = 54321;
+    const STATUS = 1;
+    const PICTURE = 1;
+    
+    const post_name = req.body.post_name
+    const description = req.body.description
+    const category = req.body.category
+    const location = req.body.location
+    const post_date = req.body.post_date
+
+    const sqlInsert = "INSERT INTO POST (post_name,user_id,item_id,description,category,post_date,post_status,location,picture) VALUES (?,?,?,?,?,?,?,?,?);"
+    db.query(sqlInsert, [post_name,USER_ID,ITEM_ID,description,category,post_date,STATUS,location,PICTURE], (err,result)=>{
+        console.log(err);
+    })
+    
+})
+
+app.listen(8000, () =>{
+    console.log("Running on port 8000");
+})
+
