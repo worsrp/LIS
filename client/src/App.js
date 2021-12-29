4688641332dc5ad6a9b31ab58875462ac8be9d0a
khampanpare88 <khampanpare88@gmail.com>
2021-12-30 00:24:19 +0700

    ยังไม่เสร็จเหลือแก้รูป

--- client/src/App.js
+++ client/src/App.js
@@ -1,70 +1,25 @@
 import logo from './logo.svg';
-import './App.css';
-import React,{useState, useEffect } from "react";
-import Axios from 'axios'
+ import './App.css';
 
-function App() {
-  const [Name,setName] = useState('')
-  const [Category,setCategory] = useState('')
-  const [Locatione,setLocation] = useState('')
-  const [Description,setDescription] = useState('')
+ function App() {
+   return (
+     <div className="App">
+       <header className="App-header">
+         <img src={logo} className="App-logo" alt="logo" />
+         <p>
+           Edit <code>src/App.js</code> and save to reload.
+         </p>
+         <a
+           className="App-link"
+           href="https://reactjs.org"
+           target="_blank"
+           rel="noopener noreferrer"
+         >
+           Learn React
+         </a>
+       </header>
+     </div>
+   );
+ }
 
-  const submitPost = () => {
-
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
-    today = yyyy + '-' + mm + '-' + dd;
-    console.log(Name);
-    Axios.post("http://localhost:8000/createpost", { 
-      post_name: Name,  
-      category: Category,
-      post_date: today,  
-      location: Locatione,  
-      description: Description
-      
-    }).then(() => {
-      alert("successful insert");
-    })
-  };
-
-  return (
-    <div className="App">
-      <h>LOVE IS SHARING</h><br></br>
-      <h>CREATE POST</h><br></br>
-      <div className="form">
-        <label>Post Name : </label>
-        <input type="text" name="post_name" onChange={(e)=>{
-          setName(e.target.value)
-        }}/><br></br>
-
-        <label>Category : </label>
-        <select name="category"  onChange={(e)=>{
-          setCategory(e.target.value)
-        }}>
-          <option value="Fashion">Fashion</option>
-          <option value="IT">IT</option>
-        </select><br></br>
-
-        <label>Location : </label>
-        <select name="location"  onChange={(e)=>{
-          setLocation(e.target.value)
-        }}>
-          <option value="Chiang Mai">Chiang Mai</option>
-          <option value="BKK">BKK</option>
-          <option value="Chiang Rai">Chiang Rai</option>
-        </select><br></br>
-
-        <label>Description : </label>
-        <input type="text" name="Description" onChange={(e)=>{
-          setDescription(e.target.value)
-        }}/><br></br>
-        <button onClick={submitPost}> POST </button>
-
-      </div>
-    </div>
-  );
-}
-
-export default App;
+ export default App;
--- server/index.js
+++ server/index.js
@@ -24,9 +24,8 @@ app.use(express.json());
 
 app.post("/createpost", (req, res) =>{
     //test
-    const USER_ID = 12345;
-    const ITEM_ID = 54321;
-    const STATUS = 1;
+    const USER_ID = 1234;
+    const STATUS = 'Available';
     const PICTURE = 1;
     
     const post_name = req.body.post_name
@@ -34,9 +33,10 @@ app.post("/createpost", (req, res) =>{
     const category = req.body.category
     const location = req.body.location
     const post_date = req.body.post_date
+    const edit_date = req.body.post_date
 
-    const sqlInsert = "INSERT INTO POST (post_name,user_id,item_id,description,category,post_date,post_status,location,picture) VALUES (?,?,?,?,?,?,?,?,?);"
-    db.query(sqlInsert, [post_name,USER_ID,ITEM_ID,description,category,post_date,STATUS,location,PICTURE], (err,result)=>{
+    const sqlInsert = "INSERT INTO POST (post_name,user_id,description,category,post_date,edit_date,post_status,location,picture) VALUES (?,?,?,?,?,?,?,?,?);"
+    db.query(sqlInsert, [post_name,USER_ID,description,category,post_date,edit_date,STATUS,location,PICTURE], (err,result)=>{
         console.log(err);
     })
     
