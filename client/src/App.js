58e316b85a36134b17c1c7232c570fd1e16259cb
worsrp <sirapop_p@cmu.ac.th>
2022-01-02 22:54:21 +0700

    client's routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
- import './App.css';
+import './custom.scss';
+import { Link, Route } from 'react-router-dom';
 
- function App() {
-   return (
-     <div className="App">
-       <header className="App-header">
-         <img src={logo} className="App-logo" alt="logo" />
-         <p>
-           Edit <code>src/App.js</code> and save to reload.
-         </p>
-         <a
-           className="App-link"
-           href="https://reactjs.org"
-           target="_blank"
-           rel="noopener noreferrer"
-         >
-           Learn React
-         </a>
-       </header>
-     </div>
-   );
- }
 
- export default App;
+//import routes
+import CreatePost from './pages/Createpost';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav class="navbar navbar-expand-lg navbar-light bg-#FFF">
+          <a class="navbar-brand" href="/"><b class="comname">Love is Sharing.</b></a>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto icon-pos">
+              <li class="nav-item">
+                <Link to="/createpost"><i class="bi bi-plus-lg icon-large icon-area"></i></Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/"><i class="bi bi-heart icon-large icon-area"></i></Link>
+              </li>
+              <li class="nav-item">
+              <Link to="/"><i class="bi bi-person-circle icon-large icon-area"></i></Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        
+        <div>
+            <Route path="/createpost">
+              <CreatePost />
+            </Route>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
