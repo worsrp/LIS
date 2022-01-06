1eaeaff1fcdd94b093784312bf51bef7dc808403
janitra <jajajanitra@gmail.com>
2022-01-06 19:25:18 +0700

    add search bar

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -11,14 +11,30 @@ const Feed = () =>{
         });
     }, []);
 
+    const searchPost = () => {
+
+    };
+
     return (
-        <div className="feed">
-            <div>
-                {feedPost.map((val)=> {
-                    return <h2> Name : {val.post_name} <br /> Description : {val.description} <br /> Location : {val.location} </h2>
-                })}
+        <div>
+            <div className="searchBar">
+                <div className="form">
+                    <input type="text" placeholder="search item" name="search" onChange={(e)=>{
+                    setSearch(e.target.value)
+                    }} required />
+
+                    <button onClick={searchPost}> search </button>
+                </div>
+            </div>
+            <div className="feed">
+                <div>
+                    {feedPost.map((val)=> {
+                        return <h2> Name : {val.post_name} <br /> Description : {val.description} <br /> Location : {val.location} </h2>
+                    })}
+                </div>
             </div>
         </div>
+        
 
     );
 
