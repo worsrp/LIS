9c509d118d0f9d2b9b5d135afcd6e98bdafde7d3
aewsunantha <59277896+aewsunantha@users.noreply.github.com>
2022-01-12 10:23:19 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: ["http://localhost:8000"],
+        origin: ["http://localhost:3000"],
         methods: ["GET", "POST"],
         credentials: true,
     })
