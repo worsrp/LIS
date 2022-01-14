b34c763f20ce484ee0d6b0cc4bfd93b0db26f1aa
worsrp <sirapop_p@cmu.ac.th>
2022-01-14 18:15:38 +0700

    update rwgister ui

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,13 @@
 import Axios from "axios";
-import React, {useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+
+//import style
+import '../custom.scss';
+import { Row, Col, Container, Image, Form, Button } from 'react-bootstrap';
+import { HiUser } from "react-icons/hi";
+import { MdError } from "react-icons/md";
+
 
 function Register (){
     const [IsError, setIsError] = useState("");         
@@ -10,7 +18,8 @@ function Register (){
     const [address, setAddress] = useState("");
     const [passwordReg, setPasswordReg] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
-
+    const [check, setCheck] = useState(false);
+    const [alertShow, setAlertShow] = useState(false);
 
     Axios.defaults.withCredentials = true;
 
@@ -28,113 +37,111 @@ function Register (){
         })
     };
 
-    const checkValidation=(e)=>{
-        setConfirmPassword(e.target.value);
-        if(passwordReg != confirmPassword){
-            setIsError("Confirm Password should be match with password");
+    useEffect(() =>{
+        if(passwordReg !== confirmPassword){
+            setIsError("Password does not match!");
+            setAlertShow(true);
         }else{
             setIsError("");
-    }
-    };
-    
+            setAlertShow(false);
+        }
+    }, [check]);
+
     return (
-        <center>
-        <div className="App Container">
-        <div className="register">
-            <h1> Register </h1>
-            <form action="">
-            <div className="mb-3">
-                <label htmlFor="name" className="form-label">
-                email:
-                </label>
-                <input
-                type="text"
-                className="form-controll"
-                onChange={(e) => {
-                    setEmailReg(e.target.value)
-                }}
-                ></input>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="name" className="form-label">
-                Firstname :
-                </label>
-                <input
-                type="text"
-                className="form-controll"
-                onChange={(e) => {
-                    setFirstname(e.target.value)
-                }}
-                ></input>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="name" className="form-label">
-                Lastname :
-                </label>
-                <input
-                type="text"
-                className="form-controll"
-                onChange={(e) => {
-                    setLastname(e.target.value)
-                }}
-                ></input>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="name" className="form-label">
-                Moblie :
-                </label>
-                <input
-                type="text"
-                className="form-controll"
-                onChange={(e) => {
-                    setMoblie(e.target.value)
-                }}
-                ></input>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="name" className="form-label">
-                Address :
-                </label>
-                <input
-                type="text"
-                className="form-controll"
-                onChange={(e) => {
-                    setAddress(e.target.value)
-                }}
-                ></input>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="name" className="form-label">
-                Password :
-                </label>
-                <input
-                type="password"
-                className="form-controll"
-                onChange={(e) => {
-                    setPasswordReg(e.target.value)
-                }}
-                ></input>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="name" className="form-label">
-                Confirm Password :
-                </label>
-                <input
-                type="password"
-                className="form-controll"
-                onChange={(e) => checkValidation(e)}
-                ></input>
-                <br />
-                <p1>{IsError}</p1>
-            </div>
-            <br />
-                <button class="btn btn-success" onClick={register}>
-                Register
-                </button>
-            </form>
-        </div>
-        </div>
-    </center>
+        <Container className="set-font">
+            <Row style={{ marginTop: '4%', marginLeft: '1%' }}>
+                <Col>
+                    <div class="text-banner">Love</div>
+                    <div class="text-banner">is Sharing.</div>
+                    <Image src={ require('../register.png') } 
+                    style={{ width: '500px', marginTop: '20%' }}/>
+                </Col>
+                <Col>
+                    <Row style={{ borderBottom: '2px dashed lightgray' }}>
+                        <Col className="text-sub-banner">Register</Col>
+                        <Col style={{ color: 'darkblue', textAlign: 'end', paddingTop: '6%' }}>
+                            <Link to="/login" style={{ textDecoration: 'none', color: 'darkblue' }}>Already a member?</Link>
+                            <HiUser className="icon-sim" style={{ marginBottom: '5px' }} />
+                        </Col>
+                    </Row>
+                    <Row style={{ marginTop: '5%', borderBottom: '2px dashed lightgray', paddingBottom: '7%' }}>
+                        <Row style={{ marginTop: '1%' }}>
+                            <Col>First name</Col>
+                            <Col>Last name</Col>
+                        </Row>
+                        <Row style={{ marginTop: '1%' }}>
+                            <Col>
+                                <Form.Control type="text" placeholder="First name" 
+                                onChange={(e) => { setFirstname(e.target.value) }}/>
+                            </Col>
+                            <Col>
+                                <Form.Control type="text" placeholder="Last name" 
+                                onChange={(e) => { setLastname(e.target.value) }}/>
+                            </Col>
+                        </Row>
+                        <Row style={{ marginTop: '4%' }}>
+                            <Col>Email</Col>
+                            <Col>Mobile</Col>
+                        </Row>
+                        <Row style={{ marginTop: '1%' }}>
+                            <Col>
+                                <Form.Control type="email" placeholder="email address" 
+                                onChange={(e) => { setEmailReg(e.target.value) }} />
+                            </Col>
+                            <Col>
+                                <Form.Control type="text" placeholder="Mobile number"
+                                onChange={(e) => { setMoblie(e.target.value) }}  />
+                            </Col>
+                        </Row>
+                        <Row style={{ marginTop: '4%' }}>
+                            <Col>Address</Col>
+                        </Row>
+                        <Row style={{ marginTop: '1%' }}>
+                            <Col>
+                                <Form.Control as="textarea" rows={2} style={{ resize: 'none' }} placeholder="Address" 
+                                onChange={(e) => { setAddress(e.target.value) }} />
+                            </Col>
+                        </Row>
+                    </Row>
+                    <Row style={{ marginTop: '5%', borderBottom: '2px dashed lightgray', paddingBottom: '7%' }}>
+                        <Row style={{ marginTop: '1%' }}>
+                            <Col>Password</Col>
+                            <Col>Confirm password</Col>
+                        </Row>
+                        <Row style={{ marginTop: '1%' }}>
+                            <Col>
+                                <Form.Control type="password" placeholder="Password" 
+                                onChange={(e) => { setPasswordReg(e.target.value) }} />
+                            </Col>
+                            <Col>
+                                <Form.Control type="password" placeholder="Confirm your password" 
+                                onChange={(e) => { 
+                                    setConfirmPassword(e.target.value);
+                                    setCheck(!check);
+                                }} />
+                            </Col>
+                        </Row>
+                        <Row style={{ marginTop: '1%' }}>
+                            <Col></Col>
+                            <Col className="text-error">
+                                {alertShow === false ? (
+                                    <span></span>
+                                    ) : (
+                                        <MdError className="icon-sim" />
+                                    )}
+                                {IsError}
+                            </Col>
+                        </Row>
+                    </Row>
+                    <Row style={{ marginTop: '5%' }}>
+                        <Col style={{  textAlign: 'end' }}>
+                        <Button style={{ backgroundColor: 'darkblue', border: 'none', width: '15%' }}
+                        onClick={ register } >Register</Button>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
